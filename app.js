9e232f249b082a7f97c79966b0d8e3103c216c7c
Viviane DEROLDO SABADOTI <viviane.deroldosabadoti2023@campus-eni.fr>
2024-03-08 13:54:19 +0100

    CRUD is working

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
+const uniqid = require('uniqid')
 
 //Creating a model with .model(the name of the model, the class, the name of the collection (table))
 const Movie = mongoose.model('Movie',
@@ -28,11 +30,17 @@ mongoose.connection.on('error', () => {
 //Start the application
 const app = express();
 
+
 //Swagger middleware
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('./swagger_outuput.json');
 
+//JSON cors
+app.use(express.json());
+app.use(cors());
+
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 
 
 //Routing
@@ -60,6 +68,59 @@ app.get('/movie/:id', async (request, response) => {
     response.json(movie);
 
 })
+
+app.post('/movie/create', async (req, res) => {
+    
+    /* 
+            #swagger.description = 'Create un film avec ID' 
+    */
+
+   let movie = new Movie();
+   movie.id = Math.round(Math.random()*10000)
+   movie.title = req.body.title
+   movie.year = req.body.year
+   movie.synopsis = req.body.synopsis
+   movie.duration = req.body.duration
+   await movie.save();
+   res.json('OK');
+    
+})
+            
+
+
+app.post('/movie/edit/:id?', async (req, response) => {
+    
+    /* 
+            #swagger.description = 'Update/create un film avec ID' 
+    */
+   /*
+            const movie = new Movie();
+            if (req.params.id) {
+                movie.id = Math.random()
+            } else {
+                const idRequest = parseInt(req.params.id);
+                movie = await Movie.findOne({ id : idRequest});
+            }
+            */
+            const idRequest = parseInt(req.params.id);
+            movie = await Movie.findOne({ id : idRequest});
+            movie.title = req.body.title;
+            movie.year = req.body.year;
+            movie.synopsis = req.body.synopsis;
+            movie.duration = req.body.duration; 
+            await movie.save();
+            response.json('OK');                  
+})
+
+app.delete('/movie/delete/:id', async (req,res) => { 
+ 
+    /* 
+            #swagger.description = 'Delete un film avec ID' 
+    */
+    await Movie.findOneAndDelete({id: req.params.id});
+    res.json("OK");
+    }); 
+    
 
 //Start the server
 app.listen(3000, () => {
